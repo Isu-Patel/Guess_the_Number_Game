9fd25bbf3640dd2983efb4b252ab764d13b6b462
ISU <110623091+Isu-Patel@users.noreply.github.com>
2023-04-30 21:40:39 +0530

    Update and rename .gitignore to Guess_the_Number_Game.py

---
+++ Guess_the_Number_Game.py
@@ -0,0 +1,20 @@
+# Guess The Numeber Game
+
+import random
+
+secret_number = random.randint(1, 100)
+
+guess = 0
+
+while guess != secret_number:
+
+    guess = int(input("Guess a number between 1 to 100: "))
+    
+    if guess > secret_number:
+        print("Too High. Guess lower.")
+    
+    elif guess < secret_number:
+        print("Too lower. Guess High.")
+    
+    else:
+        print("YAY! You Won you have guessed the right number.")
